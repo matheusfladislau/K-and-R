35764857f19d1224830e998d671fbecf4920e7e1
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2024-11-21 01:25:18 -0300

    wip: ex21.c

---
+++ chapter_01/ex21.c
@@ -0,0 +1,66 @@
+//1-21 Write a program entab that replaces strings of blanks by the minimum number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab. When either a tab or a single blank would suffice to reach a tab stop, which should be given preference?
+
+#include <stdio.h>
+#define TABSTOP 8
+
+#define OUT 0
+#define IN 1
+
+void entab(int initialpos, int position);
+
+int main() {
+    int c, position, state, initialpos;
+    position = 0;
+    state = OUT;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' && state == OUT) {
+            state = IN;
+            initialpos = position;
+            ++position;
+        } else {
+            if (state == IN && c != ' ') {
+                entab(initialpos, position);
+                state = OUT;
+            }
+
+            if (c == '\n') {
+                position = 0;
+            } else {
+                ++position;
+            }
+
+
+            if (c != ' ') {
+                putchar(c);
+            }
+        }
+    }
+    //fazer o EOF (usar o ultimo position)
+	return 0;
+}
+
+void entab(int initialpos, int position) {
+    printf("\n");
+    printf("initialpos = %d\n", initialpos);
+    printf("position = %d\n", position);
+    int amount_space_tabstop = TABSTOP - (initialpos % TABSTOP);
+    int amount_space = position - (initialpos % TABSTOP);
+    printf("\n");
+    printf("amount_space_tabstop = %d\n", amount_space_tabstop);
+    printf("amount_space = %d\n", amount_space);
+
+    if (amount_space < amount_space_tabstop) {
+        while (amount_space > 0) {
+            putchar('.');
+            --amount_space;
+        }
+    } else {
+        putchar('-');
+        amount_space -= amount_space_tabstop;
+        while (amount_space > 0) {
+            putchar('.');
+            --amount_space;
+        }
+    }
+}
